39bbab94e27d063814f5332d9a953094b40d47ff
ayrton26 <ayrton.feitosa@hotmail.com>
2017-10-17 15:29:34 -0300

    Desenho do recurso ilha

--- app/src/main/java/com/ayrton/fishing/engine/elements/Mapa.java
+++ app/src/main/java/com/ayrton/fishing/engine/elements/Mapa.java
@@ -22,6 +22,7 @@ public class Mapa {
         elementos = new int[16][9];
         elementos[0][0] = Recursos.ROCK_ID;
         elementos[10][4] = Recursos.ROCK_ID;
+        elementos[5][5] = Recursos.ISLAND_ID;
         this.tela = tela;
         this.recursos = recursos;
     }
@@ -36,6 +37,8 @@ public class Mapa {
     private void paintResource(int resourceID, int x, int y, Canvas canvas){
         switch (resourceID){
             case Recursos.ROCK_ID: canvas.drawBitmap(recursos.getRock(), x, y, null);
+                break;
+            case Recursos.ISLAND_ID: canvas.drawBitmap(recursos.getIsland(), x, y, null);
         }
     }
 }
--- app/src/main/java/com/ayrton/fishing/engine/elements/Recursos.java
+++ app/src/main/java/com/ayrton/fishing/engine/elements/Recursos.java
@@ -13,18 +13,25 @@ import com.ayrton.fishing.engine.elements.util.Tela;
 
 public class Recursos {
     public static final int ROCK_ID = 1;
+    public static final int ISLAND_ID = 2;
 
     private Bitmap rock;
+    private Bitmap island;
     private Resources resources;
 
     public Recursos(Resources res, Tela tela){
         resources = res;
         Bitmap b = BitmapFactory.decodeResource(resources, R.drawable.rocha);
         rock = Bitmap.createScaledBitmap(b, tela.getLarguraQuandrante(), tela.getAlturaQuadrante(), false);
+        b = BitmapFactory.decodeResource(resources, R.drawable.ilha);
+        island = Bitmap.createScaledBitmap(b, tela.getLarguraQuandrante()*3, tela.getAlturaQuadrante()*3, false);
     }
 
     public Bitmap getRock() {
         return rock;
     }
 
+    public Bitmap getIsland() {
+        return island;
+    }
 }
